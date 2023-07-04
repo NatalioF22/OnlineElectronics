18b8e3f2d2a6152cd5f0364cbc7775b26fda544b
NatalioF22 <nataliogomes707@gmail.com>
2023-07-04 18:07:20 -0400

    changed settings.py

--- Website/settings.py
+++ Website/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+STATIC_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
