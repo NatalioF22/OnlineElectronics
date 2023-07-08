2dd3e6b8e8fcd0f0d216bb1e2f2681c4e19bc471
Natalio Fernandes <116610989+NatalioF22@users.noreply.github.com>
2023-07-08 14:54:02 -0400

    Update views.py

--- WebApp/views.py
+++ WebApp/views.py
@@ -36,15 +36,14 @@ def login_user(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request,username=username, password = password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
-            messages.success(request, ("You have been logged in"))
+            login(request, user)
+            messages.success(request, "You have been logged in.")
             return redirect('home')
         else:
-            messages.success(request, ("There was an error loggin in, please try again..."))
+            messages.error(request, "There was an error logging in. Please try again.")
             return redirect('login')
-            
     else:
         return render(request, 'login.html', {})
     
